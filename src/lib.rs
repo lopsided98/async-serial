01754198b2f155c745bb94492b9aac31bafe4078
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-03-10 13:27:51 -0500

    Remove dependency on tokio and use async-io instead.
    Currently doesn't compile on Windows, and several non-essential features were
    removed (encode/decode, runtime port reconfiguration).

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,19 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
 
+use futures::Future;
 // Re-export serialport types and traits from mio_serial
 pub use mio_serial::{
     available_ports, new, ClearBuffer, DataBits, Error, ErrorKind, FlowControl, Parity, SerialPort,
     SerialPortBuilder, SerialPortInfo, SerialPortType, StopBits, UsbPortInfo,
 };
 
-use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use futures::io::{AsyncRead, AsyncWrite};
 
-use std::io::{Read, Result as IoResult, Write};
+use std::io::{Read, Write, self};
 use std::pin::Pin;
+use std::sync::Arc;
 use std::task::{Context, Poll};
-use std::time::Duration;
 
 #[cfg(feature = "codec")]
 mod frame;
@@ -26,7 +27,7 @@ mod frame;
 #[cfg(unix)]
 mod os_prelude {
     pub use futures::ready;
-    pub use tokio::io::unix::AsyncFd;
+    pub use async_io::Async;
 }
 
 #[cfg(windows)]
@@ -54,7 +55,7 @@ pub type Result<T> = mio_serial::Result<T>;
 #[derive(Debug)]
 pub struct SerialStream {
     #[cfg(unix)]
-    inner: AsyncFd<mio_serial::SerialStream>,
+    inner: Arc<Async<mio_serial::SerialStream>>,
     // Named pipes and COM ports are actually two entirely different things that hardly have anything in common.
     // The only thing they share is the opaque `HANDLE` type that can be fed into `CreateFileW`, `ReadFile`, `WriteFile`, etc.
     //
@@ -65,17 +66,24 @@ pub struct SerialStream {
     // The com port is kept around for serialport related methods
     #[cfg(windows)]
     com: mem::ManuallyDrop<mio_serial::SerialStream>,
+    readable: Option<async_io::ReadableOwned<mio_serial::SerialStream>>,
+    writable: Option<async_io::WritableOwned<mio_serial::SerialStream>>,
 }
 
 impl SerialStream {
     /// Open serial port from a provided path, using the default reactor.
     pub fn open(builder: &crate::SerialPortBuilder) -> crate::Result<Self> {
         let port = mio_serial::SerialStream::open(builder)?;
+        Self::new(port)
+    }
 
+    fn new(port: mio_serial::SerialStream) -> crate::Result<Self> {
         #[cfg(unix)]
         {
             Ok(Self {
-                inner: AsyncFd::new(port)?,
+                inner: Arc::new(Async::new(port)?),
+                readable: None,
+                writable: None
             })
         }
 
@@ -87,6 +95,8 @@ impl SerialStream {
             Ok(Self {
                 inner: unsafe { named_pipe::NamedPipeClient::from_raw_handle(handle)? },
                 com,
+                readable: None,
+                writable: None
             })
         }
     }
@@ -104,28 +114,9 @@ impl SerialStream {
     pub fn pair() -> crate::Result<(Self, Self)> {
         let (master, slave) = mio_serial::SerialStream::pair()?;
 
-        let master = SerialStream {
-            inner: AsyncFd::new(master)?,
-        };
-        let slave = SerialStream {
-            inner: AsyncFd::new(slave)?,
-        };
+        let master = Self::new(master)?;
+        let slave = Self::new(slave)?;
         Ok((master, slave))
-    }
-
-    /// Sets the exclusivity of the port
-    ///
-    /// If a port is exclusive, then trying to open the same device path again
-    /// will fail.
-    ///
-    /// See the man pages for the tiocexcl and tiocnxcl ioctl's for more details.
-    ///
-    /// ## Errors
-    ///
-    /// * `Io` for any error while setting exclusivity for the port.
-    #[cfg(unix)]
-    pub fn set_exclusive(&mut self, exclusive: bool) -> crate::Result<()> {
-        self.inner.get_mut().set_exclusive(exclusive)
     }
 
     /// Returns the exclusivity of the port
@@ -137,50 +128,6 @@ impl SerialStream {
         self.inner.get_ref().exclusive()
     }
 
-    /// Borrow a reference to the underlying mio-serial::SerialStream object.
-    #[inline(always)]
-    fn borrow(&self) -> &mio_serial::SerialStream {
-        #[cfg(unix)]
-        {
-            self.inner.get_ref()
-        }
-        #[cfg(windows)]
-        {
-            self.com.deref()
-        }
-    }
-
-    /// Borrow a mutable reference to the underlying mio-serial::SerialStream object.
-    #[inline(always)]
-    fn borrow_mut(&mut self) -> &mut mio_serial::SerialStream {
-        #[cfg(unix)]
-        {
-            self.inner.get_mut()
-        }
-        #[cfg(windows)]
-        {
-            self.com.deref_mut()
-        }
-    }
-    /// Try to read bytes on the serial port.  On success returns the number of bytes read.
-    ///
-    /// The function must be called with valid byte array `buf` of sufficient
-    /// size to hold the message bytes. If a message is too long to fit in the
-    /// supplied buffer, excess bytes may be discarded.
-    ///
-    /// When there is no pending data, `Err(io::ErrorKind::WouldBlock)` is
-    /// returned. This function is usually paired with `readable()`.
-    pub fn try_read(&mut self, buf: &mut [u8]) -> IoResult<usize> {
-        #[cfg(unix)]
-        {
-            self.inner.get_mut().read(buf)
-        }
-        #[cfg(windows)]
-        {
-            self.inner.try_read(buf)
-        }
-    }
-
     /// Wait for the port to become readable.
     ///
     /// This function is usually paired with `try_read()`.
@@ -188,24 +135,9 @@ impl SerialStream {
     /// The function may complete without the socket being readable. This is a
     /// false-positive and attempting a `try_read()` will return with
     /// `io::ErrorKind::WouldBlock`.
-    pub async fn readable(&self) -> IoResult<()> {
+    pub async fn readable(&self) -> io::Result<()> {
         let _ = self.inner.readable().await?;
         Ok(())
-    }
-
-    /// Try to write bytes on the serial port.  On success returns the number of bytes written.
-    ///
-    /// When the write would block, `Err(io::ErrorKind::WouldBlock)` is
-    /// returned. This function is usually paired with `writable()`.
-    pub fn try_write(&mut self, buf: &[u8]) -> IoResult<usize> {
-        #[cfg(unix)]
-        {
-            self.inner.get_mut().write(buf)
-        }
-        #[cfg(windows)]
-        {
-            self.inner.try_write(buf)
-        }
     }
 
     /// Wait for the port to become writable.
@@ -215,7 +147,7 @@ impl SerialStream {
     /// The function may complete without the socket being readable. This is a
     /// false-positive and attempting a `try_write()` will return with
     /// `io::ErrorKind::WouldBlock`.
-    pub async fn writable(&self) -> IoResult<()> {
+    pub async fn writable(&self) -> io::Result<()> {
         let _ = self.inner.writable().await?;
         Ok(())
     }
@@ -241,22 +173,30 @@ impl AsyncRead for SerialStream {
     ///
     /// This function may encounter any standard I/O error except `WouldBlock`.
     fn poll_read(
-        self: Pin<&mut Self>,
+        mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<IoResult<()>> {
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
         loop {
-            let mut guard = ready!(self.inner.poll_read_ready(cx))?;
+            // Attempt the non-blocking operation.
+            match self.inner.get_ref().read(buf) {
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock => {}
+                res => {
+                    self.readable = None;
+                    return Poll::Ready(res);
+                }
+            }
 
-            match guard.try_io(|inner| inner.get_ref().read(buf.initialize_unfilled())) {
-                Ok(Ok(bytes_read)) => {
-                    buf.advance(bytes_read);
-                    return Poll::Ready(Ok(()));
-                }
-                Ok(Err(err)) => {
-                    return Poll::Ready(Err(err));
-                }
-                Err(_would_block) => continue,
+            // Initialize the future to wait for readiness.
+            if self.readable.is_none() {
+                self.readable = Some(self.inner.clone().readable_owned());
+            }
+
+            // Poll the future for readiness.
+            if let Some(f) = &mut self.readable {
+                let res = ready!(Pin::new(f).poll(cx));
+                self.readable = None;
+                res?;
             }
         }
     }
@@ -281,30 +221,58 @@ impl AsyncWrite for SerialStream {
     /// # Errors
     ///
     /// This function may encounter any standard I/O error except `WouldBlock`.
-    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<IoResult<usize>> {
+    fn poll_write(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<io::Result<usize>> {
         loop {
-            let mut guard = ready!(self.inner.poll_write_ready(cx))?;
+            // Attempt the non-blocking operation.
+            match self.inner.get_ref().write(buf) {
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock => {}
+                res => {
+                    self.writable = None;
+                    return Poll::Ready(res);
+                }
+            }
 
-            match guard.try_io(|inner| inner.get_ref().write(buf)) {
-                Ok(result) => return Poll::Ready(result),
-                Err(_would_block) => continue,
+            // Initialize the future to wait for readiness.
+            if self.writable.is_none() {
+                self.writable = Some(self.inner.clone().writable_owned());
+            }
+
+            // Poll the future for readiness.
+            if let Some(f) = &mut self.writable {
+                let res = ready!(Pin::new(f).poll(cx));
+                self.writable = None;
+                res?;
             }
         }
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<IoResult<()>> {
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         loop {
-            let mut guard = ready!(self.inner.poll_write_ready(cx))?;
-            match guard.try_io(|inner| inner.get_ref().flush()) {
-                Ok(_) => return Poll::Ready(Ok(())),
-                Err(_would_block) => continue,
+            // Attempt the non-blocking operation.
+            match self.inner.get_ref().flush() {
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock => {}
+                res => {
+                    self.writable = None;
+                    return Poll::Ready(res);
+                }
+            }
+
+            // Initialize the future to wait for readiness.
+            if self.writable.is_none() {
+                self.writable = Some(self.inner.clone().writable_owned());
+            }
+
+            // Poll the future for readiness.
+            if let Some(f) = &mut self.writable {
+                let res = ready!(Pin::new(f).poll(cx));
+                self.writable = None;
+                res?;
             }
         }
     }
 
-    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<IoResult<()>> {
-        let _ = self.poll_flush(cx)?;
-        Ok(()).into()
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.poll_flush(cx)
     }
 }
 
@@ -314,7 +282,7 @@ impl AsyncRead for SerialStream {
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
-    ) -> Poll<IoResult<()>> {
+    ) -> Poll<io::Result<()>> {
         let mut self_ = self;
         Pin::new(&mut self_.inner).poll_read(cx, buf)
     }
@@ -322,169 +290,19 @@ impl AsyncRead for SerialStream {
 
 #[cfg(windows)]
 impl AsyncWrite for SerialStream {
-    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<IoResult<usize>> {
+    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<io::Result<usize>> {
         let mut self_ = self;
         Pin::new(&mut self_.inner).poll_write(cx, buf)
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<IoResult<()>> {
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         let mut self_ = self;
         Pin::new(&mut self_.inner).poll_flush(cx)
     }
 
-    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<IoResult<()>> {
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         let mut self_ = self;
         Pin::new(&mut self_.inner).poll_shutdown(cx)
-    }
-}
-
-impl crate::SerialPort for SerialStream {
-    #[inline(always)]
-    fn name(&self) -> Option<String> {
-        self.borrow().name()
-    }
-
-    #[inline(always)]
-    fn baud_rate(&self) -> crate::Result<u32> {
-        self.borrow().baud_rate()
-    }
-
-    #[inline(always)]
-    fn data_bits(&self) -> crate::Result<crate::DataBits> {
-        self.borrow().data_bits()
-    }
-
-    #[inline(always)]
-    fn flow_control(&self) -> crate::Result<crate::FlowControl> {
-        self.borrow().flow_control()
-    }
-
-    #[inline(always)]
-    fn parity(&self) -> crate::Result<crate::Parity> {
-        self.borrow().parity()
-    }
-
-    #[inline(always)]
-    fn stop_bits(&self) -> crate::Result<crate::StopBits> {
-        self.borrow().stop_bits()
-    }
-
-    #[inline(always)]
-    fn timeout(&self) -> Duration {
-        Duration::from_secs(0)
-    }
-
-    #[inline(always)]
-    fn set_baud_rate(&mut self, baud_rate: u32) -> crate::Result<()> {
-        self.borrow_mut().set_baud_rate(baud_rate)
-    }
-
-    #[inline(always)]
-    fn set_data_bits(&mut self, data_bits: crate::DataBits) -> crate::Result<()> {
-        self.borrow_mut().set_data_bits(data_bits)
-    }
-
-    #[inline(always)]
-    fn set_flow_control(&mut self, flow_control: crate::FlowControl) -> crate::Result<()> {
-        self.borrow_mut().set_flow_control(flow_control)
-    }
-
-    #[inline(always)]
-    fn set_parity(&mut self, parity: crate::Parity) -> crate::Result<()> {
-        self.borrow_mut().set_parity(parity)
-    }
-
-    #[inline(always)]
-    fn set_stop_bits(&mut self, stop_bits: crate::StopBits) -> crate::Result<()> {
-        self.borrow_mut().set_stop_bits(stop_bits)
-    }
-
-    #[inline(always)]
-    fn set_timeout(&mut self, _: Duration) -> crate::Result<()> {
-        Ok(())
-    }
-
-    #[inline(always)]
-    fn write_request_to_send(&mut self, level: bool) -> crate::Result<()> {
-        self.borrow_mut().write_request_to_send(level)
-    }
-
-    #[inline(always)]
-    fn write_data_terminal_ready(&mut self, level: bool) -> crate::Result<()> {
-        self.borrow_mut().write_data_terminal_ready(level)
-    }
-
-    #[inline(always)]
-    fn read_clear_to_send(&mut self) -> crate::Result<bool> {
-        self.borrow_mut().read_clear_to_send()
-    }
-
-    #[inline(always)]
-    fn read_data_set_ready(&mut self) -> crate::Result<bool> {
-        self.borrow_mut().read_data_set_ready()
-    }
-
-    #[inline(always)]
-    fn read_ring_indicator(&mut self) -> crate::Result<bool> {
-        self.borrow_mut().read_ring_indicator()
-    }
-
-    #[inline(always)]
-    fn read_carrier_detect(&mut self) -> crate::Result<bool> {
-        self.borrow_mut().read_carrier_detect()
-    }
-
-    #[inline(always)]
-    fn bytes_to_read(&self) -> crate::Result<u32> {
-        self.borrow().bytes_to_read()
-    }
-
-    #[inline(always)]
-    fn bytes_to_write(&self) -> crate::Result<u32> {
-        self.borrow().bytes_to_write()
-    }
-
-    #[inline(always)]
-    fn clear(&self, buffer_to_clear: crate::ClearBuffer) -> crate::Result<()> {
-        self.borrow().clear(buffer_to_clear)
-    }
-
-    /// Cloning SerialStream is not supported.
-    ///
-    /// # Errors
-    /// Always returns `ErrorKind::Other` with a message.
-    #[inline(always)]
-    fn try_clone(&self) -> crate::Result<Box<dyn crate::SerialPort>> {
-        Err(crate::Error::new(
-            crate::ErrorKind::Io(std::io::ErrorKind::Other),
-            "Cannot clone Tokio handles",
-        ))
-    }
-
-    #[inline(always)]
-    fn set_break(&self) -> crate::Result<()> {
-        self.borrow().set_break()
-    }
-
-    #[inline(always)]
-    fn clear_break(&self) -> crate::Result<()> {
-        self.borrow().clear_break()
-    }
-}
-
-impl Read for SerialStream {
-    fn read(&mut self, buf: &mut [u8]) -> IoResult<usize> {
-        self.try_read(buf)
-    }
-}
-
-impl Write for SerialStream {
-    fn write(&mut self, buf: &[u8]) -> IoResult<usize> {
-        self.try_write(buf)
-    }
-
-    fn flush(&mut self) -> IoResult<()> {
-        self.borrow_mut().flush()
     }
 }
 
--- tests/test_serialstream.rs
+++ tests/test_serialstream.rs
@@ -1,9 +1,6 @@
+use async_serial::SerialPortBuilderExt;
+use futures::{AsyncReadExt, AsyncWriteExt};
 use std::time::Duration;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    process, time,
-};
-use tokio_serial::SerialPortBuilderExt;
 
 #[cfg(unix)]
 const DEFAULT_TEST_PORT_NAMES: &str = concat!(
@@ -17,7 +14,7 @@ const DEFAULT_TEST_PORT_NAMES: &str = "COM10;COM11";
 
 struct Fixture {
     #[cfg(unix)]
-    process: process::Child,
+    process: std::process::Child,
     pub port_a: &'static str,
     pub port_b: &'static str,
 }
@@ -25,35 +22,27 @@ struct Fixture {
 #[cfg(unix)]
 impl Drop for Fixture {
     fn drop(&mut self) {
-        if let Some(id) = self.process.id() {
-            log::trace!("stopping socat process (id: {})...", id);
-
-            self.process.start_kill().ok();
-            std::thread::sleep(Duration::from_millis(250));
-            log::trace!("removing link: {}", self.port_a);
-            std::fs::remove_file(self.port_a).ok();
-            log::trace!("removing link: {}", self.port_b);
-            std::fs::remove_file(self.port_b).ok();
-        }
+        self.process.kill().ok();
+        std::thread::sleep(Duration::from_millis(250));
+        std::fs::remove_file(self.port_a).ok();
+        std::fs::remove_file(self.port_b).ok();
     }
 }
 
 impl Fixture {
     #[cfg(unix)]
-    pub async fn new(port_a: &'static str, port_b: &'static str) -> Self {
+    pub fn new(port_a: &'static str, port_b: &'static str) -> Self {
         let args = [
             format!("PTY,link={}", port_a),
             format!("PTY,link={}", port_b),
         ];
-        log::trace!("starting process: socat {} {}", args[0], args[1]);
 
-        let process = process::Command::new("socat")
+        let process = std::process::Command::new("socat")
             .args(&args)
             .spawn()
             .expect("unable to spawn socat process");
-        log::trace!(".... done! (pid: {:?})", process.id().unwrap());
 
-        time::sleep(Duration::from_millis(500)).await;
+        std::thread::sleep(Duration::from_millis(500));
 
         Self {
             process,
@@ -68,43 +57,39 @@ impl Fixture {
     }
 }
 
-async fn setup_virtual_serial_ports() -> Fixture {
+fn setup_virtual_serial_ports() -> Fixture {
     let port_names: Vec<&str> = std::option_env!("TEST_PORT_NAMES")
         .unwrap_or(DEFAULT_TEST_PORT_NAMES)
         .split(';')
         .collect();
 
     assert_eq!(port_names.len(), 2);
-    Fixture::new(port_names[0], port_names[1]).await
+    Fixture::new(port_names[0], port_names[1])
 }
 
-#[tokio::test]
-async fn send_recv() {
-    env_logger::init();
+#[test]
+fn send_recv() {
+    let fixture = setup_virtual_serial_ports();
+    futures::executor::block_on(async move {
+        let mut sender = async_serial::new(fixture.port_a, 9600)
+            .open_native_async()
+            .expect("unable to open serial port");
+        let mut receiver = async_serial::new(fixture.port_b, 9600)
+            .open_native_async()
+            .expect("unable to open serial port");
 
-    let fixture = setup_virtual_serial_ports().await;
+        let message = b"This is a test message";
+        sender
+            .write_all(message)
+            .await
+            .expect("unable to write test message");
 
-    let mut sender = tokio_serial::new(fixture.port_a, 9600)
-        .open_native_async()
-        .expect("unable to open serial port");
-    let mut receiver = tokio_serial::new(fixture.port_b, 9600)
-        .open_native_async()
-        .expect("unable to open serial port");
+        let mut buf = [0u8; 32];
+        receiver
+            .read_exact(&mut buf[..message.len()])
+            .await
+            .expect("unable to read test message");
 
-    log::trace!("sending test message");
-    let message = b"This is a test message";
-    sender
-        .write_all(message)
-        .await
-        .expect("unable to write test message");
-
-    log::trace!("receiving test message");
-    let mut buf = [0u8; 32];
-    let n = receiver
-        .read_exact(&mut buf[..message.len()])
-        .await
-        .expect("unable to read test message");
-
-    log::trace!("checking test message");
-    assert_eq!(&buf[..n], message);
+        assert_eq!(&buf[..message.len()], message);
+    })
 }
